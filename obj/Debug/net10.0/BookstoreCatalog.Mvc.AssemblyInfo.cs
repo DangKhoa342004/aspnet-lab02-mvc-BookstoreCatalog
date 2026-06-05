bb4b15eeaecf3f76483a78b54a4ac616739fabf5
DangKhoa <khoand6055@gmail.com>
2026-06-05 15:22:02 +0700

    init: create asp.net lab2 mvc BookstoreCatalog

--- obj/Debug/net10.0/BookstoreCatalog.Mvc.AssemblyInfo.cs
+++ obj/Debug/net10.0/BookstoreCatalog.Mvc.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookstoreCatalog.Mvc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5bbfaba97ac56a6fab6c5c8652f08e12f3d17619")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a593546a26f2b81db8950087cb062a1123ab508")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookstoreCatalog.Mvc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookstoreCatalog.Mvc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
