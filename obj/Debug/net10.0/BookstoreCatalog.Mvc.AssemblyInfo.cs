c7aa0635c9bad5627fcb47a43136f1f5c4218b55
DangKhoa <khoand6055@gmail.com>
2026-06-08 02:07:09 +0700

    feat: add create and search, update view models

--- obj/Debug/net10.0/BookstoreCatalog.Mvc.AssemblyInfo.cs
+++ obj/Debug/net10.0/BookstoreCatalog.Mvc.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookstoreCatalog.Mvc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dfe475cd81fd1adf780fdd27a4e8c88b67450966")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a00bb6dcd06a13e569b94f43e80217e563e56cd")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookstoreCatalog.Mvc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookstoreCatalog.Mvc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
